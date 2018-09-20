d139cf554f3d4103623abc6ff2989a6a6af24dd6
joshgressman <joshgressman@gmail.com>
2018-09-20 13:41:25 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,39 @@
+// Define UI Variables
+const form = document.querySelector("#task-form");
+const taskList = document.querySelector(".collection");
+const clearBtn = document.querySelector(".clear-tasks");
+const filter = document.querySelector("#filter");
+const taskInput = document.querySelector("#task");
+
+// Load all event listeners
+loadEventListeners();
+
+//Load all event listeners
+function loadEventListeners() {
+  //Add task event
+  form.addEventListener("submit", addTask);
+}
+
+// Add Task
+function addTask(e) {
+  if (taskInput.value === "") {
+    alert("Add a task");
+  }
+
+  //Create li element
+  const li = document.createElement("li");
+  //Add Class
+  li.className = "collection-item";
+  //Create text node and append to li
+  li.appendChild(document.createTextNode(taskInput.value));
+
+  //Create new link element
+  const link = document.createElement("a");
+  link.className = "delete-item secondary-content";
+  //Add icon html
+  link.innerHTML = '<i class="fa fa-remove"></i>';
+  //Append link to the li
+  li.appendChild(link);
+
+  e.preventDefault();
+}
