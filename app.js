4806dc0820ded46c279c6e75e6a62164fef35546
joshgressman <joshgressman@gmail.com>
2018-09-21 14:15:07 -0500

    added filter method

--- app.js
+++ app.js
@@ -18,6 +18,9 @@ function loadEventListeners() {
 
   //Cleat all tasks event
   clearBtn.addEventListener('click', clearTasks)
+
+  //Filter task events
+  filter.addEventListener('keyup', filterTasks)
 }
 
 // Add Task
@@ -70,4 +73,18 @@ function clearTasks(e) {
    taskList.removeChild(taskList.firstChild);
  }
 
+}
+
+//Filter task function based onmatches
+function filterTasks (e) {
+  const text = e.target.value.toLowerCase();
+
+  document.querySelectorAll('.collection-item').forEach(function(task){
+    const item = task.firstChild.textContent;
+    if(item.toLowerCase().indexOf(text) != -1){
+      task.style.display = 'block';
+    } else {
+      task.style.display = 'none';
+    }
+  });
 }
